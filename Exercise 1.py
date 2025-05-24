1f0dcfdc902cd37da391c9438f961cd98b615908
Tony <etiennearthurdubois@gmail.com>
2025-05-24 12:33:00 -0400

    Exercise #1 - Hello, world!
    Exercise Task(s):
    Write a program that prints your full name and your birthday as separate strings.
    Write a program that shows the use of all 7 arithmetic operations.

---
+++ Exercise 1.py
@@ -0,0 +1,34 @@
+# INFORMATION
+#
+#   Title: Practice Exercise #1 - 'Hello, World!'
+#   Version: 1.00
+#   Author: neocodedit
+#   Created: 2025-05-24
+#
+#
+# DESCRIPTION
+#
+#   Exercises
+#   1 - Write a program that prints your full name and your birthday
+#   as separate strings.
+#   2 - Write a program that shows the use of all 7 arithmetic operations.
+#
+#
+# UPDATES INDEX
+#
+#   (2025-05-24)(1.00): Program creation.
+
+
+print('Hello, world! My name is neocodedit.', 'I was born on 24-05-2025.')
+print()
+print('Here is a list of what I can do with arithmetic operations!')
+print()
+print('Additions: 5 + 5 =',(5+5))
+print('Substractions: 15 - 5 =',(15-5))
+print('Multiplications: 5 x 5 =',(5*5))
+print('Divisions: 14 % 3 =',(14/3))
+print('Divisions (Interger): 14 % 3 =',(14//3))
+print('Exponentials: 5² =',(5**2))
+print('Remainders (Modulo): Remainder of 14 % 3 =',(14%3))
+
+
