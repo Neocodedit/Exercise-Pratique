3eb181997da4aa70f87185d69507a9a68a6020ec
Tony <etiennearthurdubois@gmail.com>
2025-05-24 12:55:51 -0400

    #2 - Who Goes There?
    Exercise Task(s):
    Write a program that gets 2 string variables and 2 number variables from the user, concatenates (joins them together with no spaces) and displays the strings, then multiplies the two numbers on a new line.

---
+++ Exercise 2.py
@@ -0,0 +1,29 @@
+# INFORMATION
+#
+#   Title: Practice Exercise #2 - 'Who goes there?'
+#   Version: 1.00
+#   Author: neocodedit
+#   Created: 2025-05-24
+#
+#
+# DESCRIPTION
+#
+#   Exercises
+#   1 - Write a program that gets 2 string variables
+#   and 2 number variables from the user.
+#   concatenates (joins them together with no spaces)
+#   and displays the strings, then multiplies the two numbers on a new line.
+#
+#
+# UPDATES INDEX
+#
+#   (2025-05-24)(1.00): Program creation.
+
+
+string1 = str(input('First word: '))
+string2 = str(input('Second word: '))
+number1 = int(input('First number: '))
+number2 = int(input('Second number: '))
+print()
+print(string1 + string2)
+print(number1,'x',number2,'=',(number1 * number2))
